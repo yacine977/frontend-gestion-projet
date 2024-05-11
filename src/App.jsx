f862918218f16b210575e53f0880d9bf123dba8e
yacine977 <yacine.tamindjoute97@gmail.com>
2024-05-11 14:28:56 +0200

    Création du composant ListeDesProjets et affichage de la liste des projets depuis le backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import ListeDesProjets from './composants/ListeDesProjets';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -17,19 +18,9 @@ function App() {
         </a>
       </div>
       <h1>Application de Gestion de Projet</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <ListeDesProjets />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/composants/ListeDesProjets.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+
+function ListeDesProjets() {
+  const [projets, setProjets] = useState([]);
+
+ useEffect(() => {
+  fetch('http://localhost:3000/projet')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Erreur HTTP: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(data => setProjets(data))
+    .catch(error => {
+      console.error('Une erreur est survenue lors de la récupération des projets : ', error);
+    });
+}, []);
+
+  return (
+    <div>
+      {projets.map(projet => (
+        <div key={projet.id}>
+          <h2>{projet.nom}</h2>
+          <p>{projet.description}</p>
+          <p>Date de début : {new Date(projet.dateDebut).toLocaleDateString()}</p>
+          <p>Date de fin prévue : {new Date(projet.dateFinPrevu).toLocaleDateString()}</p>
+          <p>Date de fin réelle : {projet.dateFinReel ? new Date(projet.dateFinReel).toLocaleDateString() : 'N/A'}</p>
+          <p>Chef de projet ID : {projet.chefDeProjetId}</p>
+          <p>Est validé : {projet.est_valide ? 'Oui' : 'Non'}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default ListeDesProjets;
