733ae552446533dbbc0d4e345b9b1c59190b21e5
yacine977 <yacine.tamindjoute97@gmail.com>
2024-05-28 02:45:33 +0200

    Ajout de la fonctionnalité de suppression de réunion

--- src/composants/ListeReunions.jsx
+++ src/composants/ListeReunions.jsx
@@ -10,8 +10,27 @@ function ListeReunions() {
       .catch((error) => console.error(error));
   }, []);
 
+  const supprimerReunion = async (id) => {
+    if (!window.confirm("Voulez-vous vraiment supprimer cette réunion ?")) {
+      return;
+    }
+    const response = await fetch(`http://localhost:3000/reunion/${id}`, {
+      method: "DELETE",
+    });
+
+    if (response.ok) {
+      alert("Réunion supprimée avec succès");
+      setReunions(reunions.filter((reunion) => reunion.id !== id));
+    } else {
+      alert("Erreur lors de la suppression de la réunion");
+    }
+  }
+
+
   return (
     <div>
+      
+      
       <h1>Liste des réunions</h1>
       {reunions.map((reunion) => (
         <div key={reunion.id}>
@@ -19,6 +38,7 @@ function ListeReunions() {
           <p>Date et heure : {reunion.dateTime}</p>
           <p>ID du projet : {reunion.projetId}</p>
           <p>ID du créateur : {reunion.createurId}</p>
+          <button onClick={() => supprimerReunion(reunion.id)}>Supprimer</button>
         </div>
       ))}
     </div>
