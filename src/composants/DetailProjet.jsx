553aa5b6584e0dfc30acbebb9f630a2c8c5ada85
yacine977 <yacine.tamindjoute97@gmail.com>
2024-05-30 01:43:12 +0200

    Ajout de l'authentification à la requête fetch dans DetailProjet

--- src/composants/DetailProjet.jsx
+++ src/composants/DetailProjet.jsx
@@ -6,22 +6,26 @@ function DetailProjet() {
   const { id } = useParams();
 
   useEffect(() => {
-    fetch(`http://localhost:3000/projet/${id}`)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`Erreur HTTP: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then(data => {
-        console.log(data); // Ajout de console.log ici
-        const [projet] = data; // Déstructure le tableau pour obtenir le premier élément
-        setProjet(projet);
-      })
-      .catch(error => {
-        console.error('Une erreur est survenue lors de la récupération du projet : ', error);
-      });
-  }, [id]);
+  fetch(`http://localhost:3000/projet/${id}`, {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Erreur HTTP: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+      const [projet] = data;
+      setProjet(projet);
+    })
+    .catch(error => {
+      console.error('Une erreur est survenue lors de la récupération du projet : ', error);
+    });
+}, [id]);
 
   if (!projet) {
     return <div>Chargement...</div>;
