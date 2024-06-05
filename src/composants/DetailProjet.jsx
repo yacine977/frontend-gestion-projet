7134ac27669d0eb2c4b567eeeb7260ce8544311e
yacine977 <yacine.tamindjoute97@gmail.com>
2024-06-05 17:54:50 +0200

    Amélioration du style du composant DetailProjet

--- src/composants/DetailProjet.jsx
+++ src/composants/DetailProjet.jsx
@@ -1,31 +1,35 @@
-import { useState, useEffect } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import "../styles/DetailProjet.css";
 
 function DetailProjet() {
   const [projet, setProjet] = useState(null);
   const { id } = useParams();
 
   useEffect(() => {
-  fetch(`http://localhost:3000/projet/${id}`, {
-    headers: {
-      Authorization: `Bearer ${localStorage.getItem("token")}`,
-    },
-  })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`Erreur HTTP: ${response.status}`);
-      }
-      return response.json();
+    fetch(`http://localhost:3000/projet/${id}`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
     })
-    .then(data => {
-      console.log(data);
-      const [projet] = data;
-      setProjet(projet);
-    })
-    .catch(error => {
-      console.error('Une erreur est survenue lors de la récupération du projet : ', error);
-    });
-}, [id]);
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Erreur HTTP: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        const [projet] = data;
+        setProjet(projet);
+      })
+      .catch((error) => {
+        console.error(
+          "Une erreur est survenue lors de la récupération du projet : ",
+          error
+        );
+      });
+  }, [id]);
 
   if (!projet) {
     return <div>Chargement...</div>;
@@ -40,31 +44,20 @@ function DetailProjet() {
       <p>Date de fin réelle : {projet.dateFinReel ? new Date(projet.dateFinReel).toLocaleDateString() : 'Non défini'}</p>
       <p>Chef de projet ID : {projet.chefDeProjetId}</p>
       <p>Est validé : {projet.est_valide ? 'Oui' : 'Non'}</p>
-      <Link to={`/taches-projet/${projet.id}`} className='button'>
-        Voir les tâches de ce projet
-      </Link>
-      <br/>
-      <Link to={`/creer-tache/${projet.id}`} className='button'>
-  Créer une tâche pour ce projet
-</Link>
-<br/>
-<Link to={`/creer-document/${projet.id}`} className='button'>
-  Créer un document pour ce projet
-</Link>
-<br/>
-<Link to={`/documents-projet/${projet.id}`} className='button'>
-  Voir les documents de ce projet
-</Link>
-      
-      
-      
-      
-      
-
-      
-      
-      
-
+      <div className='button-group'> {/* Ajoutez la classe 'button-group' à la div */}
+        <Link to={`/taches-projet/${projet.id}`} className='button'>
+          Voir les tâches de ce projet
+        </Link>
+        <Link to={`/creer-tache/${projet.id}`} className='button'>
+          Créer une tâche pour ce projet
+        </Link>
+        <Link to={`/creer-document/${projet.id}`} className='button'>
+          Créer un document pour ce projet
+        </Link>
+        <Link to={`/documents-projet/${projet.id}`} className='button'>
+          Voir les documents de ce projet
+        </Link>
+      </div>
     </div>
   );
 }
