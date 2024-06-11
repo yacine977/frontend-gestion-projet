a359a091c80a4f1cd15db4c4056fcc85fcf20c31
yacine977 <yacine.tamindjoute97@gmail.com>
2024-06-11 23:37:37 +0200

    Ajout de styles au composant UsersList

--- src/composants/UsersList.jsx
+++ src/composants/UsersList.jsx
@@ -1,28 +1,36 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
+import "../styles/UserList.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUserCheck,
+  faProjectDiagram,
+} from "@fortawesome/free-solid-svg-icons";
 
 function UserList() {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:3000/utilisateur/allUsers') // Remplacez par l'URL de votre serveur
-      .then(response => response.json())
-      .then(data => setUsers(data))
+    fetch("http://localhost:3000/utilisateur/allUsers")
+      .then((response) => response.json())
+      .then((data) => setUsers(data))
       .catch((error) => {
-        console.error('Erreur : ', error);
+        console.error("Erreur : ", error);
       });
   }, []);
 
   const getRole = async (uid) => {
-    const response = await fetch(`http://localhost:3000/utilisateur/getRole/${uid}`);
+    const response = await fetch(
+      `http://localhost:3000/utilisateur/getRole/${uid}`
+    );
     const data = await response.json();
     return data.role;
   };
 
   const setRole = async (uid, role) => {
-    const response = await fetch('http://localhost:3000/utilisateur/setRole', {
-      method: 'POST',
+    const response = await fetch("http://localhost:3000/utilisateur/setRole", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({ uid, role }),
     });
@@ -31,40 +39,59 @@ function UserList() {
   };
 
   const assignUsersToProject = async (projectId, uid) => {
-    const response = await fetch(`http://localhost:3000/projet/${projectId}/assignerFirebase`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${localStorage.getItem("token")}`
-      },
-      body: JSON.stringify({ utilisateurs: [uid] }),
-    });
+    const response = await fetch(
+      `http://localhost:3000/projet/${projectId}/assignerFirebase`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+        body: JSON.stringify({ utilisateurs: [uid] }),
+      }
+    );
     const data = await response.json();
     alert(data.message);
   };
 
   return (
-    <div>
+    <div className="userListContainer">
       <h1>Liste des utilisateurs</h1>
       {users.map((user) => (
-        <div key={user.uid}>
+        <div className="userCard" key={user.uid}>
           <h2>{user.displayName}</h2>
           <p>Email: {user.email}</p>
           <p>UID: {user.uid}</p>
-          <select onChange={(e) => setRole(user.uid, e.target.value)}>
-            <option value=""> Definir un role </option>
+          <select
+            className="roleSelector"
+            onChange={(e) => setRole(user.uid, e.target.value)}
+          >
+            <option value="">Définir un rôle</option>
             <option value="PDG">PDG</option>
             <option value="ChefDeProjet">Chef de projet</option>
             <option value="UtilisateurStandard">Utilisateur standard</option>
-            <option value="AdministrateurInfrastructure">Administrateur d'infrastructure</option>
+            <option value="AdministrateurInfrastructure">
+              Administrateur d'infrastructure
+            </option>
           </select>
-          <button onClick={() => getRole(user.uid).then(role => alert(`Role: ${role}`))}>Voir le rôle</button>
-          <button onClick={() => {
-            const projectId = prompt("Entrez l'ID du projet");
-            if (projectId) {
-              assignUsersToProject(projectId, user.uid);
+          <button
+            className="roleButton"
+            onClick={() =>
+              getRole(user.uid).then((role) => alert(`Role: ${role}`))
             }
-          }}>Assigner au projet</button>
+          >
+            <FontAwesomeIcon icon={faUserCheck} /> Voir le rôle
+          </button>
+          <button
+            className="assignButton"
+            onClick={() => {
+              const projectId = prompt("Entrez l'ID du projet");
+              if (projectId) {
+                assignUsersToProject(projectId, user.uid);
+              }
+            }}>
+            <FontAwesomeIcon icon={faProjectDiagram} /> Assigner au projet
+          </button>
         </div>
       ))}
     </div>
