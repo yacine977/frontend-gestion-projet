8dc74e20c8ffe24e996a6db4bb3204f1126026ed
yacine977 <yacine.tamindjoute97@gmail.com>
2024-06-19 12:50:12 +0200

    Récupération de l'UID depuis l'utilisateur connecté pour la création de réunions

--- src/composants/CreerReunion.jsx
+++ src/composants/CreerReunion.jsx
@@ -1,21 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
-// Composant pour créer une nouvelle réunion
 function CreerReunion() {
-  // États pour gérer les champs du formulaire
   const [sujet, setSujet] = useState("");
   const [dateTime, setDateTime] = useState("");
   const [createurId, setCreateurId] = useState("");
-  // Récupération de l'ID du projet depuis l'URL
   const { projetId: initialProjetId } = useParams();
   const [projetId, setProjetId] = useState(initialProjetId || 0);
 
-  // Gestionnaire de soumission du formulaire
+  useEffect(() => {
+    // Récupération de l'ID du créateur depuis le localStorage
+    const uid = localStorage.getItem("uid");
+    if (uid) {
+      setCreateurId(uid);
+    }
+  }, []);
+
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    // Envoi des données du formulaire au serveur
     const response = await fetch("http://localhost:3000/reunion", {
       method: "POST",
       headers: {
@@ -24,19 +27,17 @@ function CreerReunion() {
       body: JSON.stringify({ sujet, dateTime, projetId, createurId }),
     });
 
-    // Réinitialisation des champs du formulaire en cas de succès
     if (response.ok) {
       alert("Réunion créée avec succès");
       setSujet("");
       setDateTime("");
-      setProjetId(initialProjetId || 0); // Réinitialisation avec l'ID du projet initial ou 0
-      setCreateurId("");
+      setProjetId(initialProjetId || 0);
+      // Pas besoin de réinitialiser createurId ici car il est récupéré du localStorage
     } else {
       alert("Erreur lors de la création de la réunion");
     }
   };
 
-  // Rendu du formulaire de création de réunion
   return (
     <form onSubmit={handleSubmit}>
       <label>
@@ -66,9 +67,10 @@ function CreerReunion() {
       <label>
         ID du créateur:
         <input
-          type="number"
+          type="text"
           value={createurId}
           onChange={(e) => setCreateurId(e.target.value)}
+          readOnly // Ce champ est en lecture seule car la valeur est pré-remplie
         />
       </label>
       <button type="submit">Créer une réunion</button>
