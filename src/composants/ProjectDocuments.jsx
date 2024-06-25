ddcebbc90cacde76b56822941a746e1a24b14b8f
yacine977 <yacine.tamindjoute97@gmail.com>
2024-06-26 01:07:54 +0200

    Ajout de la fonctionnalité de suppression d'un document dans ProjectDocuments

--- src/composants/ProjectDocuments.jsx
+++ src/composants/ProjectDocuments.jsx
@@ -5,35 +5,56 @@ function ProjectDocuments() {
   const [documents, setDocuments] = useState([]);
   const { projetId } = useParams();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(`http://localhost:3000/document/projet/${projetId}`);
-        if (!response.ok) {
-          throw new Error('La requête a échoué');
-        }
-        const data = await response.json();
-        setDocuments(data);
-      } catch (error) {
-        console.error("Erreur lors de la récupération des documents:", error);
+  // Déplacez fetchData ici pour qu'elle soit accessible dans useEffect et supprimerDocument
+  const fetchData = async () => {
+    try {
+      const response = await fetch(`http://localhost:3000/document/projet/${projetId}`);
+      if (!response.ok) {
+        throw new Error('La requête a échoué');
       }
-    };
+      const data = await response.json();
+      setDocuments(data);
+    } catch (error) {
+      console.error("Erreur lors de la récupération des documents:", error);
+    }
+  };
 
+  useEffect(() => {
     fetchData();
   }, [projetId]);
+
+  const supprimerDocument = async (documentId) => {
+    console.log("Attempting to delete document with id:", documentId);
+    if (window.confirm("Êtes-vous sûr de vouloir supprimer ce document ?")) {
+      try {
+        const response = await fetch(`http://localhost:3000/document/${documentId}`, {
+          method: "DELETE",
+        });
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const result = await response.json();
+        console.log(result.message);
+        fetchData(); // Maintenant fetchData est accessible ici
+      } catch (error) {
+        console.error("Failed to delete document:", error);
+      }
+    }
+  };
 
   return (
     <div>
       <h1>Documents du projet {projetId}</h1>
       {documents.map((document) => (
         <div key={document.id}>
-          
           <h2>{document.nom}</h2>
           <p>Type: {document.type}</p>
           <p>Chemin d'accès: {document.cheminAcces}</p>
           <p>ID de l'utilisateur: {document.utilisateurId}</p>
           <p>ID du projet: {document.projetId}</p>
-
+          <button onClick={() => supprimerDocument(document.documentId)}>Supprimer</button>
         </div>
       ))}
     </div>
